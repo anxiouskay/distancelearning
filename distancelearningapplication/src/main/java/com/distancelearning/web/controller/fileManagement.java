d121eac69c5f18e60efc2cb892c1c5123c2df4eb
Kosha Patel <koshapatel1045@gmail.com>
2018-11-15 23:36:50 -0500

    First commit
    Uploading all files

---
+++ distancelearningapplication/src/main/java/com/distancelearning/web/controller/fileManagement.java
@@ -0,0 +1,13 @@
+package com.distancelearning.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class fileManagement {
+	
+	@RequestMapping(value="index.php")
+	public String index() {
+		return "hello";
+	}
+}
